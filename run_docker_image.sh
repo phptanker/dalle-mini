e79199bdf06816b31168837ae546d0ac197874cd
phptanker <28569416+phptanker@users.noreply.github.com>
2022-06-16 03:43:24 +0200

    Config: Edit run_docker_image.sh

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script is used to run the docker image. Change or remove GPU flag if you dont have nvidia-docker or the needed GPUs
-docker run --rm --name dallemini -it -p 8888:8888  --gpus all  -v "${PWD}":/workspace dalle-mini:latest
+docker run --rm --name dallemini -it -p 8888:8888  --gpus '"device=0"'  -v D:/dev/dalle-mini/:/workspace dalle-mini:latest
